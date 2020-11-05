afa1cf890b5d5f4a00c577a8e6bf1fcd60629ad0
오태관 <73802342+ohtaegwan03@users.noreply.github.com>
2020-11-05 15:20:59 +0900

    버튼을 이용한 함수 실습

--- ex_test.js
+++ ex_test.js
@@ -1,5 +1,14 @@
+<!DOCTYPE HTML>
+<html>
 <body>
     <script>
         //배경을 붉은색으로 변경하기
+        <input_type = "button" onclick = "showMessage()">
+            <script>
+       function showMessage(){
+           alert ("message");
+            
+       }
     </script>
 </body>
+</html>
